62c3679a1a8c6d92ed906c01d660a775cd5e4c24
MohamedSaafan <201827254@std.sci.cu.edu.eg>
2022-07-19 16:48:54 +0200

    added webpackNodeExternals to the server webpack config file

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const merge = require("webpack-merge");
 const baseConfig = require("./webpack.base.js");
+const webpackNodeExternals = require("webpack-node-externals");
 const config = {
   target: "node",
   entry: "./src/index.js",
@@ -8,6 +9,7 @@ const config = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "build"),
   },
+  externals: [webpackNodeExternals()],
 };
 
 module.exports = merge(baseConfig, config);
