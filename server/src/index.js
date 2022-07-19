01db4fcd48c6226530fe494904371b65b77b7504
MohamedSaafan <201827254@std.sci.cu.edu.eg>
2022-07-19 16:54:35 +0200

    added renderer helper module

--- server/src/index.js
+++ server/src/index.js
@@ -1,24 +1,10 @@
 import express from "express";
-import Home from "./client/components/Home";
-import { renderToString } from "react-dom/server";
-import React from "react";
+import renderer from "./helpers/renderer";
 const app = express();
 
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-  const content = renderToString(<Home />);
-  console.log("hello");
-  const html = `
-    <html>
-      <head>
-      </head>
-      <body>
-      <div id = "root">${content}</div>
-      <script src = 'bundle.js'></script>
-      </body>
-    </html>
-
-  `;
+  const html = renderer();
   res.send(html);
 });
 app.listen(3000, () => console.log("server running successfully"));
