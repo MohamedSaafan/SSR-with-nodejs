1b959a27bb81b666544e7819ca658d72e0b63585
MohamedSaafan <201827254@std.sci.cu.edu.eg>
2022-07-19 15:31:44 +0200

    created the first component and built a client bundle and returned it to the browser

---
+++ server/src/client/components/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Home = () => {
+  return (
+    <div>
+      <button onClick={() => conosle.log("Clicked")}>Click Me!</button> I am the
+      home component{" "}
+    </div>
+  );
+};
+export default Home;
